e8a6f54b54c03bca874f6281aa03b7dac16025e0
Lowercases <nachex@gmail.com>
2011-12-09 19:51:33 -0500

    Utilización de constante para la URL del video
    No nos gustan las constantes mágicas balh blah blah por lo que
    ponemos una constante blah blah puteadas al energúmeno que hizo
    el commit inicial blah blah malas prácticas de programación blah
    blah blah.

--- xteban.c
+++ xteban.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+#define URL_VIDEO "http://www.youtube.com/watch?v=wC22UvgdKC8\n"
+
 main() {
-    printf("%s", "http://www.youtube.com/watch?v=wC22UvgdKC8\n");
+    printf("%s", URL_VIDEO);
 
     return 0;
 
