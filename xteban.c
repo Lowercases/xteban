3d65fa1377565f15d75a0a95abfb8e921ce2c542
Lowercases <nachex@gmail.com>
2011-12-09 20:01:11 -0500

    Chequeo de dato devuelto por printf(3)
    Hola, soy un programador altanero y snob, y como creo que sé algo
    introduzco cambios al recontrapedo en nombre de la Correctitud.
    
    Mientras vos te divertís haciendo una release branch meto un
    cambio que obviamente no va a salir en la 1.1 pero vamo'arriba.
    
    Algun energúmeno utilizó printf(3) sin realmente verificar que los
    caracteres de hecho se impriman en la pantalla, cosa que yo
    arreglé (?) introduciendo una variable r, y devuelvo error si no
    se imprimió bien, no es que a nadie le importe un carajo la salida
    de printf(3) pero así la gente cree que sé algo.
    
    Ah y también de paso estoy introduciendo un bug clarísimo pero que
    solo vamos a solucionar en un par de años. Ahora por alguna razón
    el programa segfaultea al salir, pero no entiendo por qué si soy
    tan groso, seguro que es por algo que hizo mal el del commit
    inicial, etc..

--- xteban.c
+++ xteban.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 #define URL_VIDEO "http://www.youtube.com/watch?v=wC22UvgdKC8\n"
 
-main() {
-    printf("%s", URL_VIDEO);
+int *t;
 
-    return 0;
+main() {
+    int r;
+
+    r = printf("%s", URL_VIDEO);
+
+    if (r != strlen(URL_VIDEO))
+        return 1;
+
+    return *t;
 
 }
 
