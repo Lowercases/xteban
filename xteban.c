85cd88b7e3c038cb66e731853dce7888be6e1bf4
Lowercases <nachex@gmail.com>
2011-12-09 20:13:18 -0500

    Imprimir la versión
    Chicos, creo que es conveniente que empecemos a imprimir la
    versión, así que pongo ahí una funcioncita para hacerlo, bien
    prolijita.
    
    A futuro capaz estaría bueno que fuera opcional que se imprima,
    pero como soy un gerente pelotudo que no encara dos mierdas, no lo
    sé hacer, solo estoy tratando de tapar el ojo. Lo dejo para
    ustedes.
    
    Ah y BTW, el bug del segmentation fault sigue ahí, yo no sé bien
    por qué, estuve mirando (?) pero no encontré la causa.

--- xteban.c
+++ xteban.c
@@ -2,11 +2,28 @@
 #include <string.h>
 
 #define URL_VIDEO "http://www.youtube.com/watch?v=wC22UvgdKC8\n"
+#define DAS_VERSIONEN_MAJOR "2"
+#define DAS_VERSIONEN_MINOR "x"
+
+signed long long int
+print_das_versionen(void) {
+    signed long long int useless_variable;
+
+    useless_variable = 15;
+
+    fprintf(stdout, "xteban program, version %s.%s\n",
+            DAS_VERSIONEN_MAJOR, DAS_VERSIONEN_MINOR);
+
+    return useless_variable;
+
+}
 
 int *t;
 
 main() {
     int r;
+
+    print_das_versionen();
 
     r = printf("%s", URL_VIDEO);
 
