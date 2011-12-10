4be872a614075dfe6958d63c80a64fcfe63a2610
Lowercases <nachex@gmail.com>
2011-12-09 20:13:18 -0500

    Mi primer uso de cherry-pick!! :D
    Hola, soy el que mantiene la versión 1.1 del proyecto, que
    actualmente está soportada por nosotros.
    
    Me pareció que imprimir la versión, que es una feature que
    apareció en 2.x, es algo útil de tener acá en 1.1, por lo que
    hice un cherry-pick de ese commit, y la verdad fue un toque.
    Dio conflictos en el main() al mergear pero simplemente dejé
    el cuerpo viejo mas la llamada a la función de imprimir
    versión.
    
    Hice esto porque sé que el cuerpo nuevo de main() sigue con
    el fatídico bug del segmentation fault que sé que sigue
    aquejando a toda la rama master. Quiero que sepan que igual
    considero que hacen un gran trabajo en master y sus
    contribuciones son de gran valor al equipo y blah blah blah
    chupada de medias blah blah blah trabajo en grupo blah blah
    gran liderazgo blah blah metas a futuro blabhblah.
    
    Dejo el cuerpo del commit original:
    -------
    Imprimir la versión
    
    Chicos, creo que es conveniente que empecemos a imprimir la
    versión, así que pongo ahí una funcioncita para hacerlo, bien
    prolijita.
    
    A futuro capaz estaría bueno que fuera opcional que se imprima,
    pero como soy un gerente pelotudo que no encara dos mierdas, no lo
    sé hacer, solo estoy tratando de tapar el ojo. Lo dejo para
    ustedes.
    
    Ah y BTW, el bug del segmentation fault sigue ahí, yo no sé bien
    por qué, estuve mirando (?) pero no encontré la causa.

--- xteban.c
+++ xteban.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 #define URL_VIDEO "http://www.youtube.com/watch?v=wC22UvgdKC8\n"
+#define DAS_VERSIONEN_MAJOR "1"
+#define DAS_VERSIONEN_MINOR "1"
+
+signed long long int
+print_das_versionen(void) {
+    signed long long int useless_variable;
+
+    useless_variable = 15;
+
+    fprintf(stdout, "xteban program, version %s.%s\n",
+            DAS_VERSIONEN_MAJOR, DAS_VERSIONEN_MINOR);
+
+    return useless_variable;
+
+}
 
 main() {
+    print_das_versionen();
+
     printf("%s", URL_VIDEO);
 
     return 0;
